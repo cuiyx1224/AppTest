8a29bd05703e7f7f1b2d9e96e410b4a593a4ead9
cuiyingxie <abcuyx@yahoo.co.jp>
2020-09-03 11:17:44 +0900

    update

--- AppTest/Program.cs
+++ AppTest/Program.cs
@@ -7,6 +7,7 @@ namespace AppTest
         static void Main(string[] args)
         {
             Console.WriteLine("I like this ");
+			Console.WriteLine("I like this too");
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
